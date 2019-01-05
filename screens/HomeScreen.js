9378d5239e4c165f83e4930b1358cf00cb8e0d51
Lakshay Tyagi <lakshaytyagi0001@gmail.com>
2019-01-05 11:16:31 +0530

    5th

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, Button} from 'react-native';
 import {TestComponent} from './../components/AppComponents';
+import * as firebase from 'firebase';
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
     header: null,
   };
 
+  onSignoutPress = () => {
+    firebase.auth().signOut();
+  }
+
   render() {
     return (
       <View style={{paddingTop:20}}>
       <Text> HomeScreen </Text>
       <TestComponent/>
+      < Button title='Signout' onPress={this.onSignoutPress}/>
       </View>
     );
   }
